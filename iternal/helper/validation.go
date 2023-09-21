407e71e2f4a547902d133a41e79c6e3b9c2cd30c
MaximysUa <102983194+MaximysUa@users.noreply.github.com>
2023-09-21 09:36:35 +0300

    Update validation.go
    Write some code into github

--- iternal/helper/validation.go
+++ iternal/helper/validation.go
@@ -9,9 +9,32 @@ import (
 	"net/http"
 	persondto "people-food-service/iternal/person/dto"
 )
-
+// Validationt name and familyName
+// It should be not empty and consist only alhpabet characters
 func Validation(r *http.Request) (persondto.RequestDTO, error) {
+	req, err := renderToDTO(r)
+	if err != nil{
+		return req, err	
+	}
 
+	if err := validator.New().Struct(req); err != nil {
+		validateErr := err.(validator.ValidationErrors)
+
+		return req, fmt.Errorf("invalid request. Error: %v", validateErr)
+
+	}
+	return req, nil
+}
+//TODO write the validatoin of uuid
+func ValidationUUID(r *http.Request) (persondto.RequestDTO, error){
+	req, err := renderToDTO(r)
+	if err != nil{
+		return req, err	
+	}
+
+	return req, nil
+}
+func renderToDTO(r *http.Request) (persondto.RequestDTO, error){
 	var req persondto.RequestDTO
 	err := render.DecodeJSON(r.Body, &req)
 	if errors.Is(err, io.EOF) {
@@ -22,12 +45,4 @@ func Validation(r *http.Request) (persondto.RequestDTO, error) {
 
 		return req, fmt.Errorf("failed to decode request body. Error: %v", err)
 	}
-
-	if err := validator.New().Struct(req); err != nil {
-		validateErr := err.(validator.ValidationErrors)
-
-		return req, fmt.Errorf("invalid request. Error: %v", validateErr)
-
-	}
-	return req, nil
 }
